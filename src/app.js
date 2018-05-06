0857799df1bda806bb0e7c819c7117842e1943fe
Charpell <charpellumeh@gmail.com>
2018-05-06 21:54:25 +0100

    - Connect the Store to the component using connect function - Use Mapstate to Props to get the state from the store and pass it as props to the component

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import { createStore } from 'redux';
 import reducers from './reducers';
 
 import { Header } from './components/common/Header';
+import LibraryList from './components/LibraryList';
 
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
     <Provider store={createStore(reducers)}>
       <View>
         <Header headerText="Tech Stack" />
+        <LibraryList />
       </View>
     </Provider>
   )
